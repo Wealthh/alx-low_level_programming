0082613d0cf7ad986071b08a5727fd3a0a8ce3e6
Wealthh <petuayoola001@gmail.com>
2023-02-16 19:40:11 -0800

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -This is a program that prints a line to the standard error
- * Return: 1 (success)
+ * main -This program is written in C
+ * Return 1 (succes)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout");
-        return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
