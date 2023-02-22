2529e66d2a16036ba89585c21475bb98461a09d7
Wealthh <petuayoola001@gmail.com>
2023-02-21 16:55:18 -0800

    tired

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * main -a program that prints alphabet in lowercase
- * Return: always 0
+ * print_alphabet -function that prints alphabet in lower case
+ *
+ * Return: always 0;
  */
 
 void print_alphabet(void)
